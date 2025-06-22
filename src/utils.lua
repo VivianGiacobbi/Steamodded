8eae8b1c75e152583eb022690b5956f889948446
Richard <richardbailey@outlook.com>
2025-06-23 00:16:52 +0100

    Fix functionality

--- src/utils.lua
+++ src/utils.lua
@@ -2499,7 +2499,7 @@ G.FUNCS.update_blind_debuff_text = function(e)
     if not e.config.object then return end
     local new_str = SMODS.debuff_text or G.GAME.blind:get_loc_debuff_text()
     if new_str ~= e.config.object.string then
-        e.config.object.string = new_str
+        e.config.object.config.string = {new_str}
         e.config.object:update_text(true)
         e.UIBox:recalculate()
     end
